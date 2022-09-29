fca7a959a2f05d2b6823b971149bb7ca03f1efc6
oddaf <oddaf@proton.me>
2022-09-29 13:25:46 -0300

    safe engine with debt rewards

---
+++ src/DebtRewards.sol
@@ -0,0 +1,181 @@
+pragma solidity 0.6.7;
+
+abstract contract TokenLike {
+    function balanceOf(address) virtual public view returns (uint256);
+    function transfer(address, uint256) virtual external returns (bool);
+}
+abstract contract RewardDripperLike {
+    function dripReward() virtual external;
+    function rewardPerBlock() virtual external view returns (uint256);
+    function rewardToken() virtual external view returns (TokenLike);
+}
+
+// @notice Do not use tokens with transfer callbacks with this contract
+contract DebtRewards {
+    // Staked Supply (== sum of all debt balances)
+    uint256                     public totalDebt; 
+    // Amount of rewards per share accumulated (total, see rewardDebt for more info)
+    uint256                     public accTokensPerShare;
+    // Balance of the rewards token in this contract since last update
+    uint256                     public rewardsBalance;    
+    // Last block when a reward was pulled
+    uint256                     public lastRewardBlock;    
+    // Balances
+    mapping(address => uint256) public debtBalanceOf;
+    // The amount of tokens inneligible for claiming rewards (see formula below)
+    mapping(address => uint256) internal rewardDebt;
+    // Pending reward = (descendant.balanceOf(user) * accTokensPerShare) - rewardDebt[user]    
+
+    // Contract that drips rewards
+    RewardDripperLike immutable public rewardDripper;        
+    // Reward Token
+    TokenLike         immutable public rewardToken;       
+
+    // --- Events ---
+    event AddAuthorization(address account);
+    event RemoveAuthorization(address account);
+    event DebtSet(address indexed safe, uint256 amount);
+    event RewardsPaid(address account, uint256 amount);
+    event PoolUpdated(uint256 accTokensPerShare, uint256 stakedSupply);    
+
+    // --- Auth ---
+    mapping (address => uint) public authorizedAccounts;
+    /**
+     * @notice Add auth to an account
+     * @param account Account to add auth to
+     */
+    function addAuthorization(address account) virtual external isAuthorized {
+        authorizedAccounts[account] = 1;
+        emit AddAuthorization(account);
+    }
+
+    /**
+     * @notice Remove auth from an account
+     * @param account Account to remove auth from
+     */
+    function removeAuthorization(address account) virtual external isAuthorized {
+        authorizedAccounts[account] = 0;
+        emit RemoveAuthorization(account);
+    }
+
+    /**
+    * @notice Checks whether msg.sender can call an authed function
+    **/
+    modifier isAuthorized {
+        require(authorizedAccounts[msg.sender] == 1, "DebtRewards/account-not-authorized");
+        _;
+    }
+
+    // --- Math ---
+    uint256 public constant WAD = 10 ** 18;
+    uint256 public constant RAY = 10 ** 27;
+
+    function addition(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x + y) >= x, "DebtRewards/add-overflow");
+    }
+    function subtract(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x - y) <= x, "DebtRewards/sub-underflow");
+    }
+    function multiply(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, "DebtRewards/mul-overflow");
+    }
+
+    constructor(
+        address rewardDripper_
+    ) public {
+        require(rewardDripper_ != address(0), "DebtRewards/null-reward-dripper");
+
+        rewardDripper = RewardDripperLike(rewardDripper_);
+        rewardToken   = RewardDripperLike(rewardDripper_).rewardToken();
+
+        authorizedAccounts[msg.sender] = 1;
+        emit AddAuthorization(msg.sender);
+    }
+
+    /*
+    * @notice Returns unclaimed rewards for a given user
+    */
+    function pendingRewards(address user) public view returns (uint256) {
+        uint accTokensPerShare_ = accTokensPerShare;
+        if (block.number > lastRewardBlock && totalDebt != 0) {
+            uint increaseInBalance = multiply(subtract(block.number, lastRewardBlock), rewardDripper.rewardPerBlock());
+            accTokensPerShare_ = addition(accTokensPerShare_, multiply(increaseInBalance, RAY) / totalDebt);
+        }
+        return subtract(multiply(debtBalanceOf[user], accTokensPerShare_) / RAY, rewardDebt[user]);
+    }
+
+    /*
+    * @notice Returns rewards earned per block for each token deposited (WAD)
+    */
+    function rewardRate() public view returns (uint256) {
+        if (totalDebt == 0) return 0;
+        return multiply(rewardDripper.rewardPerBlock(), WAD) / totalDebt;
+    }
+
+    // --- Core Logic ---
+    /*
+    * @notify Updates the pool and pays rewards (if any)
+    * @dev Must be included in deposits and withdrawals
+    */
+    modifier payRewards(address who) {
+        updatePool();
+
+        if (debtBalanceOf[who] > 0 && rewardToken.balanceOf(address(this)) > 0) {
+            // Pays the reward
+            uint256 pending = subtract(multiply(debtBalanceOf[who], accTokensPerShare) / RAY, rewardDebt[who]);
+
+            require(rewardToken.transfer(who, pending), "DebtRewards/could-not-transfer-reward-token");
+            rewardsBalance = rewardToken.balanceOf(address(this));
+
+            emit RewardsPaid(who, pending);
+        }
+        _;
+        rewardDebt[who] = multiply(debtBalanceOf[who], accTokensPerShare) / RAY;
+    }
+
+    /*
+    * @notify Pays outstanding rewards to msg.sender
+    */
+    function getRewards() external payRewards(msg.sender) {}
+
+    /*
+    * @notify Pull funds from the dripper
+    */
+    function pullFunds() public {
+        rewardDripper.dripReward();
+    }
+
+    /*
+    * @notify Updates pool data
+    */
+    function updatePool() public {
+        if (block.number <= lastRewardBlock) return;
+        lastRewardBlock = block.number;
+        if (totalDebt == 0) return;
+
+        pullFunds();
+        uint256 increaseInBalance = subtract(rewardToken.balanceOf(address(this)), rewardsBalance);
+        rewardsBalance = addition(rewardsBalance, increaseInBalance);
+
+        // Updates distribution info
+        accTokensPerShare = addition(accTokensPerShare, multiply(increaseInBalance, RAY) / totalDebt);
+        emit PoolUpdated(accTokensPerShare, totalDebt);
+    }
+
+    /*
+    * @notify Set a safe debt
+    * @param who Owner of the safe
+    * @param wad Current debt of the safe
+    * @dev Only safeEngine can call this function
+    */
+    function setDebt(address who, uint256 wad) external payRewards(who) isAuthorized {
+        if (debtBalanceOf[who] > wad)
+            totalDebt = subtract(totalDebt, debtBalanceOf[who] - wad);
+        else
+            totalDebt = addition(totalDebt, wad - debtBalanceOf[who]);
+        
+        debtBalanceOf[who] = wad;
+
+        emit DebtSet(who, wad);
+    }
+}
---
+++ test/DebtRewards.t.sol
@@ -0,0 +1,220 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.6.7;
+
+import "forge-std/Test.sol";
+import "ds-token/token.sol";
+import "../src/DebtRewards.sol";
+import {AutoRewardDripper} from "../src/AutoRewardDripper.sol";
+
+contract SAFEEngineMock {
+    DebtRewards immutable rewards;
+
+    constructor(address rewards_) public {
+        rewards = DebtRewards(rewards_);
+    }
+
+    function setDebt(address who, uint256 wad) external {
+        rewards.setDebt(who, wad);
+    }
+}
+
+contract DebtRewardsTest is Test {
+    DSToken rewardToken;
+    DebtRewards debtRewards;
+    SAFEEngineMock safeEngine;
+    AutoRewardDripper rewardDripper;
+
+    // for auto dripper
+    uint256 rewardTimeline = 172800;
+    uint256 rewardCalculationDelay = 7 days;
+
+    function setUp() public {
+        vm.roll(5000000);
+        vm.warp(100000001);
+
+        rewardToken = new DSToken("PROT", "PROT");
+        rewardDripper = new AutoRewardDripper(
+            address(this),        // requestor
+            address(rewardToken),
+            rewardTimeline,
+            rewardCalculationDelay
+        );
+
+        debtRewards = new DebtRewards(address(rewardDripper));
+        
+        safeEngine = new SAFEEngineMock(address(debtRewards));
+        debtRewards.addAuthorization(address(safeEngine));
+        debtRewards.addAuthorization(address(this));
+
+        rewardDripper.modifyParameters("requestor", address(debtRewards));
+        rewardToken.mint(address(rewardDripper), 10000000 ether);
+        rewardDripper.recomputePerBlockReward();
+    }
+
+    function test_setup() public {
+        assertEq(address(debtRewards.rewardDripper()), address(rewardDripper));
+        assertEq(address(debtRewards.rewardToken()), address(rewardToken));
+        assertEq(debtRewards.authorizedAccounts(address(this)), 1);
+        assertEq(debtRewards.totalDebt(), 0);
+    }
+
+    function test_setup_null_dripper() public {
+        vm.expectRevert(bytes("DebtRewards/null-reward-dripper"));
+        debtRewards = new DebtRewards(address(0));        
+    }
+
+    function test_set_debt(address who, uint wad, uint wad2) public {
+        safeEngine.setDebt(who, wad);
+        assertEq(debtRewards.debtBalanceOf(who), wad);
+        assertEq(debtRewards.totalDebt(), wad);
+
+        safeEngine.setDebt(who, wad2);
+        assertEq(debtRewards.debtBalanceOf(who), wad2);
+        assertEq(debtRewards.totalDebt(), wad2);        
+    }
+
+    function test_debt_rewards_1(address who, uint amount, uint blockDelay) public {
+        amount = amount % 10**24 + 1; // up to 1mm staked
+        blockDelay = (blockDelay % 100) + 1; // up to 1000 blocks
+        uint rewardPerBlock = rewardDripper.rewardPerBlock();
+        // join
+        safeEngine.setDebt(who, amount);
+
+        vm.roll(block.number + blockDelay);
+        uint previousBalance = rewardToken.balanceOf(address(who));
+
+        // exit
+        safeEngine.setDebt(who, 0);
+
+        assertTrue(rewardToken.balanceOf(address(who)) >= previousBalance + (blockDelay * rewardPerBlock) - 1);
+        assertEq(debtRewards.debtBalanceOf(who), 0);
+    }
+
+    function test_debt_rewards_2_users(uint amount) public {
+        amount = amount % 10**24 + 1; // non null up to 1mm
+        address user1 = address(1);
+        address user2 = address(2);
+
+        uint rewardPerBlock = rewardDripper.rewardPerBlock();
+
+        // join
+        safeEngine.setDebt(user1, amount);
+        assertEq(debtRewards.debtBalanceOf(user1), amount);
+        safeEngine.setDebt(user2, amount);
+        assertEq(debtRewards.debtBalanceOf(user2), amount);
+
+        // exit
+        vm.roll(block.number + 32); // 32 blocks
+        uint previousBalance1 = rewardToken.balanceOf(address(user1));
+        uint previousBalance2 = rewardToken.balanceOf(address(user2));
+
+        safeEngine.setDebt(user1, 0);
+        assertEq(debtRewards.debtBalanceOf(user1), 0);
+        safeEngine.setDebt(user2, 0);
+        assertEq(debtRewards.debtBalanceOf(user2), 0);
+        
+        assertTrue(rewardToken.balanceOf(address(user1)) >= previousBalance1 + 16 * rewardPerBlock -1);
+        assertTrue(rewardToken.balanceOf(address(user1)) <= previousBalance1 + 16 * rewardPerBlock +1);
+        assertTrue(rewardToken.balanceOf(address(user2)) >= previousBalance2 + 16 * rewardPerBlock -1);
+        assertTrue(rewardToken.balanceOf(address(user2)) <= previousBalance2 + 16 * rewardPerBlock +1);
+    }
+
+    function test_get_rewards() public {
+        uint amount = 10 ether;
+        address user1 = address(1);
+
+        uint rewardPerBlock = rewardDripper.rewardPerBlock();
+
+        vm.roll(block.number + 10);
+
+        debtRewards.updatePool(); // no effect
+
+        // join
+        rewardToken.approve(address(debtRewards), uint(-1));
+        safeEngine.setDebt(user1, amount);
+
+        uint previousBalance = rewardToken.balanceOf(address(user1));
+
+        vm.roll(block.number + 10); // 10 blocks
+
+        vm.prank(user1);
+        debtRewards.getRewards();
+        assertEq(rewardToken.balanceOf(address(user1)), previousBalance + 20 * rewardPerBlock); // 1 eth per block
+
+        vm.roll(block.number + 8); // 8 blocks
+
+        vm.prank(user1);
+        debtRewards.getRewards();
+        assertTrue(rewardToken.balanceOf(address(user1)) >= previousBalance + 28 * rewardPerBlock - 1); // 1 eth per block, division rounding causes a slight loss of precision
+    }
+
+    function test_rewards_dripper_depleated2() public {
+        address user1 = address(1);
+        uint amount = 7 ether;
+        uint rewardPerBlock = rewardDripper.rewardPerBlock();
+        // leave rewards only for 20 blocks
+        rewardDripper.transferTokenOut(address(0x0ddaf), rewardToken.balanceOf(address(rewardDripper)) - 20 * rewardPerBlock);
+
+        safeEngine.setDebt(user1, amount);
+
+        vm.roll(block.number + 32); // 32 blocks
+        uint previousBalance = rewardToken.balanceOf(address(user1));
+        safeEngine.setDebt(user1, 0);           
+        
+        assertEq(debtRewards.debtBalanceOf(address(user1)), 0);
+        assertTrue(rewardToken.balanceOf(address(user1)) >= previousBalance + 20 * rewardPerBlock - 1); // full amount
+    }
+
+    function test_rewards_dripper_depleated_recharged() public {
+        address user1 = address(1);
+        uint amount = 7 ether;
+        uint rewardPerBlock = rewardDripper.rewardPerBlock();
+        // leave rewards only for 20 blocks
+        rewardDripper.transferTokenOut(address(0x0ddaf), rewardToken.balanceOf(address(rewardDripper)) - 20 * rewardPerBlock);
+
+        safeEngine.setDebt(user1, amount);
+
+        vm.roll(block.number + 32); // 32 blocks
+        uint previousBalance = rewardToken.balanceOf(address(this));
+        vm.prank(user1);
+        debtRewards.getRewards();
+        assertTrue(rewardToken.balanceOf(address(user1)) >= previousBalance + 20 * rewardPerBlock - 1); // full amount
+
+        vm.roll(block.number + 32); // 32 blocks
+
+        rewardToken.mint(address(rewardDripper), 5 * rewardPerBlock);
+        vm.prank(user1);
+        debtRewards.getRewards();
+        assertTrue(rewardToken.balanceOf(address(user1)) >= previousBalance + 25 * rewardPerBlock - 1);
+    }
+
+    function test_get_rewards_externally_funded() public {
+        uint amount = 10 ether;
+
+        rewardDripper.transferTokenOut(address(0xfab), rewardToken.balanceOf(address(rewardDripper)));
+
+        vm.roll(block.number + 10);
+
+        debtRewards.updatePool(); // no effect
+
+        // join
+        safeEngine.setDebt(address(this), amount);
+
+        vm.roll(block.number + 10); // 10 blocks
+
+        rewardToken.mint(address(debtRewards), 10 ether); // manually filling up contract
+
+        uint previousBalance = rewardToken.balanceOf(address(this));
+        debtRewards.getRewards();
+        assertEq(rewardToken.balanceOf(address(this)), previousBalance + 10 ether); // 1 eth per block + externally funded
+
+        vm.roll(block.number + 4);
+        debtRewards.pullFunds(); // pulling rewards to contract without updating
+
+        vm.roll(block.number + 4);
+        rewardToken.mint(address(debtRewards), 2 ether); // manually filling up contract
+
+        debtRewards.getRewards();
+        assertEq(rewardToken.balanceOf(address(this)), previousBalance + 12 ether); // 1 eth per block, division rounding causes a slight loss of precision
+    }
+}
